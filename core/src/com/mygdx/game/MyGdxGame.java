2d248154fb3729b98b6cda9fd6bb67b1d3659177
mike7c2 <mike7c2@googlemail.com>
2015-10-17 22:31:36 +0100

    Add a main loop style to render allowing drawing and physics to run separately

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,9 +2,13 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.TimeUtils;
+
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -12,16 +16,47 @@ public class MyGdxGame extends ApplicationAdapter {
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.setColor(Color.RED);
 	}
+    private BitmapFont font;
+	private double accumulator;
+	private double currentTime;
+	private float step = 1.0f / 60.0f;
+
+    private int count = 0;
+
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+		double newTime = TimeUtils.millis() / 1000.0;
+		double frameTime = Math.min(newTime - currentTime, 0.25);
+		float deltaTime = (float) frameTime;
+
+        accumulator += deltaTime;
+
+		while ( accumulator >= step){
+			accumulator -= step;
+            count += 1;
+
+			//Step the game here
+        }
+
+        //Do the drawing here
+
+        //Clear the screen
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //Redraw all the guff
 		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+        font.draw(batch, "Hello World " + Integer.toString(count), 200 + (count % 200), 200 + (count % 200));
+
+        batch.end();
+
+        System.out.println("Woo loop! " + Integer.toString(count));
+
 	}
 }
